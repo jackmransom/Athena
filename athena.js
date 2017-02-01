e04be38b2edc9bc7c0a3cb67309c9274e34cc1e5
Jack Ransom <jackmransom@gmail.com>
2017-02-01 00:52:49 +0000

    Hello, world

---
+++ athena.js
@@ -0,0 +1,15 @@
+var express = require('express')
+var app = express()
+
+var port = 8080
+
+app.get('/', function(req, res) {
+  res.send('Hello, World!')
+})
+
+app.get('/update' function(req, res) {
+})
+
+app.listen(port, function() {
+  console.log('We\'re live on port ' + port + '\n') 
+})
