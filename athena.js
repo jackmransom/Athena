759f313a2deaaa5f8434d8d2e25dd7ca441a04d0
Jack Ransom <jackmransom@gmail.com>
2017-02-01 01:19:11 +0000

    Start using Marked

--- athena.js
+++ athena.js
@@ -1,15 +1,20 @@
-var express = require('express')
-var app = express()
+//TODO: Create templates
+//TODO: Create stylesheet
+const express = require('express');
+const marked = require('marked');
+const app = express();
 
-var port = 8080
+let port = 8080; //TODO: Specify port in config file
 
 app.get('/', function(req, res) {
-  res.send('Hello, World!')
+  res.send(marked('Hello, Marked!'));
+  //TODO: Generate listing of last (x) posts
 })
 
 app.get('/update', function(req, res) {
+
 })
 
 app.listen(port, function() {
-  console.log('We\'re live on port ' + port) 
+  console.log('We\'re live on port ' + port);
 })
