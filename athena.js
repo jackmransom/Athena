e9f68c75f9019726f4e49a2ff52084a818167d67
Jack Ransom <jackmransom@gmail.com>
2017-02-07 01:39:53 +0000

    Iterate over article folder, render simple page to markdown

--- athena.js
+++ athena.js
@@ -3,12 +3,26 @@
 const express = require('express');
 const marked = require('marked');
 const app = express();
+const fs = require('fs');
 
 let port = 8080; //TODO: Specify port in config file
 
+function getListOfArticles(year) {
+  let filePath = 'posts/' + year;
+  //TODO: Use async functions
+  let articles = fs.readdirSync(filePath)
+  return articles;
+}
+
 app.get('/', function(req, res) {
-  res.send(marked('Hello, Marked!'));
+  let current_date = new Date();
+  let current_year = current_date.getFullYear();
+
   //TODO: Generate listing of last (x) posts
+  let articles = getListOfArticles(current_year);
+
+  let foo = fs.readFileSync('posts/about.md', encoding='utf8');
+  res.send(marked(foo));
 })
 
 app.get('/update', function(req, res) {
