d9b3be5153c9820d843a00b3b64d86ff86eddf39
Jack Ransom <jackmransom@gmail.com>
2017-04-23 19:48:16 +0100

    Added route for about page

--- athena.js
+++ athena.js
@@ -1,21 +1,23 @@
 //TODO: Create stylesheet
+//TODO: Decide on templating engine
 const express = require('express');
 const marked = require('marked');
 const fs = require('fs');
 const app = express();
 
-let port = process.env.PORT || 8080; //TODO: Specify port in config file
+const port = process.env.PORT || 8080; //TODO: Specify port in config file?
 
 app.use(express.static('public'));
 
 function getListOfArticles(year) {
   let filePath = 'posts/' + year;
   //TODO: Use async functions
-  let articles = fs.readdirSync(filePath)
+  let articles = fs.readdirSync(filePath);
   return articles;
 }
 
 function getFirstParagraph(year, article) {
+  //TODO: Get rid of this and just leave the full article on the page?
   let path = 'posts/' + year + '/' + article
   let file = fs.readFileSync(path, encoding='utf8');
   return file;
@@ -27,24 +29,22 @@ app.get('/', function(req, res) {
   let current_month = current_date.getMonth();
 
   //TODO: Generate listing of last (x) posts, grab the title and first paragraph for each
-  //TODO: Decide on templating engine
   let header = fs.readFileSync('templates/header.html', encoding='utf8');
   var body = "";
-
   getListOfArticles(current_year).forEach(function(article) {
     let preview = getFirstParagraph(current_year, article);
-    body = body.concat(marked(preview));
+    body = body.concat(marked(preview) + '<hr>');
     
   });
-  
   let footer = fs.readFileSync('templates/footer.html', encoding='utf8');
   res.send(header+body+footer);
 })
 
-app.get('/update', function(req, res) {
-  //TODO: Remove this?
+app.get('/about', function(req, res) {
+  let path = 'posts/about.md'
+  let file = fs.readFileSync(path, encoding='utf8')
+  res.send(marked(file))
 })
-
 
 app.listen(port, function() {
   console.log('We\'re live on port ' + port);
