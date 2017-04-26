703c0509a2deae133c6443dd9af30c3bd308f1bc
Jack Ransom <jackmransom@gmail.com>
2017-04-26 14:04:36 +0100

    Use handlebar for HTML templates

--- athena.js
+++ athena.js
@@ -1,13 +1,15 @@
 //TODO: Create stylesheet
-//TODO: Decide on templating engine
 const express = require('express');
 const marked = require('marked');
 const fs = require('fs');
 const app = express();
 const hljs = require('highlight.js')
+const handlebars = require('handlebars')
 
 const port = process.env.PORT || 8080; //TODO: Specify port in config file?
 const postsDir = 'posts/'
+const header = fs.readFileSync('templates/header.html', encoding='utf8');
+const footer = fs.readFileSync('templates/footer.html', encoding='utf8');
 
 marked.setOptions({
   highlight: function(code, lang) {
@@ -17,6 +19,40 @@ marked.setOptions({
 
 app.use(express.static('public'));
 
+
+app.get('/', function(req, res) {
+  let current_date = new Date();
+  let current_year = current_date.getFullYear();
+  let current_month = current_date.getMonth();
+
+  var body = "";
+  getListOfArticles(current_year).forEach(function(article) {
+    let preview = getArticle(current_year, article);
+    body = body.concat(marked(preview) + '<hr>');
+    
+  });
+  res.send(header+body+footer);
+})
+
+app.get('/about', function(req, res) {
+  let path = postsDir + 'about.md'
+  let file = fs.readFileSync(path, encoding='utf8')
+  res.send(header+marked(file)+footer)
+})
+
+app.get('/:year/:postName', function(req, res) {
+  let path = postsDir + req.params['year'] + '/' + req.params['postName'] + '.md'
+  let post = fs.readFileSync(path, encoding='utf8')
+  var template = handlebars.compile(header)
+  var context = {Foo: 'Foop', bar:'Baaaar'}
+  
+  res.send(template(context)+marked(post)+footer)
+})
+
+app.listen(port, function() {
+  console.log('We\'re live on port ' + port);
+})
+
 function getListOfArticles(year) {
   let filePath = postsDir + year;
   //TODO: Use async functions
@@ -24,43 +60,8 @@ function getListOfArticles(year) {
   return articles;
 }
 
-function getFirstParagraph(year, article) {
-  //TODO: Get rid of this and just leave the full article on the page?
+function getArticle(year, article) {
   let path = postsDir + year + '/' + article
   let file = fs.readFileSync(path, encoding='utf8');
   return file
 }
-
-app.get('/', function(req, res) {
-  let current_date = new Date();
-  let current_year = current_date.getFullYear();
-  let current_month = current_date.getMonth();
-
-  //TODO: Generate listing of last (x) posts, grab the title and first paragraph for each
-  let header = fs.readFileSync('templates/header.html', encoding='utf8');
-  var body = "";
-  getListOfArticles(current_year).forEach(function(article) {
-    let preview = getFirstParagraph(current_year, article);
-    body = body.concat(marked(preview) + '<hr>');
-    
-  });
-  let footer = fs.readFileSync('templates/footer.html', encoding='utf8');
-  res.send(header+body+footer);
-})
-
-app.get('/about', function(req, res) {
-  let path = postsDir + 'about.md'
-  let file = fs.readFileSync(path, encoding='utf8')
-  res.send(marked(file))
-})
-
-app.get('/:year/:postName', function(req, res) {
-  let path = postsDir + req.params['year'] + '/' + req.params['postName'] + '.md'
-  let post = fs.readFileSync(path, encoding='utf8')
-  res.send(marked(post))
-})
-
-app.listen(port, function() {
-  console.log('We\'re live on port ' + port);
-})
-
